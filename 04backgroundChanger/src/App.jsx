14d3f3d34e90d7703385b09f4c9ca5ec75975c47
arbinlama1 <tamangarbin442@gmail.com>
2025-10-01 15:45:00 +0545

    Make a small project by implementing tailwindcss and react hooks(useState)

---
+++ 04backgroundChanger/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive");
+  return (
+   <div className='w-full h-screen duration-200' 
+   style={{backgroundColor: color}}>
+   <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+    <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 rounded-xl py-2'>
+      <button className='outline-none px-4 rounded-full text-white shadow-lg'
+      style={{backgroundColor:"red"}} onClick={() => setColor("red")}>Red</button>
+
+      <button className='outline-none px-4 rounded-full text-black shadow-lg'
+      style={{backgroundColor:"yellow"}} onClick={() => setColor("yellow")}>Yellow</button>
+
+      <button className='outline-none px-4 rounded-full text-black shadow-lg'
+      style={{backgroundColor:"pink"}} onClick={() => setColor('pink')}>Pink</button>
+
+      <button className='outline-none px-4 rounded-full text-black shadow-lg'
+      style={{backgroundColor:"aqua"}} onClick={() => setColor('aqua')}>Aqua</button>
+
+      <button className='outline-none px-4 rounded-full text-white shadow-lg'
+      style={{backgroundColor:"blue"}} onClick={() => setColor('blue')}>Blue</button>
+
+      <button className='outline-none px-4 rounded-full text-white shadow-lg'
+      style={{backgroundColor:"orange"}} onClick={() => setColor('orange')}>Orange</button>
+    </div>
+   </div>
+   </div>
+  )
+}
+
+export default App
